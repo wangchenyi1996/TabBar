91f3c6af0930797e3fc15d8846eade5a93ef0e33
wangchenyi1996 <2845189781@qq.com>
2020-06-19 17:21:07 +0800

    Signed-off-by: wangchenyi1996 <2845189781@qq.com>

--- pages/common-page/goods.vue
+++ pages/common-page/goods.vue
@@ -1,35 +1,65 @@
 <template>
-	<view class="wrap">
-		<view class="item" v-for="i in 11" :key="i">
-			<view class="image">flex 布局最后一行问题</view>
+	<view>
+		<view class="ends">flex 布局最后一行问题</view>
+		<view class="wrap">
+			<view class="item" v-for="i in images" :key="i">
+				<image class="image" :src="i" mode="widthFix" />
+				
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	
+export default {
+	data() {
+		return {
+			images: [
+				'/static/images/d.png',
+				'/static/images/e.png',
+				'/static/images/g.png',
+				'/static/images/i.png',
+				'/static/images/j.png',
+				'/static/images/l.png',
+				'/static/images/m.png',
+				'/static/images/n.png',
+				'/static/images/o.png',
+				'/static/images/p.png',
+			]
+		}
+	},
+}
 </script>
 <style lang="scss">
-$padding: 20rpx;
-.wrap{
-	height: 100%;
-	width: 100%;
-	background-color: green;
-	
-	padding: $padding;
-	display: flex;
-	flex-wrap: wrap;
-	.item{
-		width: 33.33%;
+	.ends {
+		font-size: 40rpx;
+		text-align: center;
+		margin: 30rpx 0;
+	}
+
+	$padding: 20rpx;
+
+	.wrap {
+		height: 100%;
+		width: 100%;
+		background-color: green;
+
+		padding: $padding;
 		display: flex;
-		margin-bottom: 30rpx;
-		padding: 0 $padding;
-		.image{
-			height: 200rpx;
-			width: 100%;
-			background-color: red;
-			color: #FFFFFF;
+		flex-wrap: wrap;
+
+		.item {
+			width: 33.33%;
+			display: flex;
+			margin-bottom: 30rpx;
+			padding: 0 $padding;
+
+			.image {
+				height: 200rpx;
+				width: 100%;
+				// background-color: red;
+				color: #FFFFFF;
+			}
 		}
 	}
-}
 </style>
